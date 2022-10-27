2cf428027b0876bcff4878aa3cd67a4a83156c79
Erik_ <116588653+Erik44b@users.noreply.github.com>
2022-10-27 13:54:16 -0400

    Ur welcome

--- Juno C/Program.cs
+++ Juno C/Program.cs
@@ -1,12 +1,25 @@
-﻿var Bootloader = 0;
+var Bootloader = 0;
+var Version = "27";
+var VersionInt = 27;
 Console.WriteLine("Juno C");
 Console.WriteLine("Version 27");
-if (Bootloader == 27)
+if (Bootloader == VersionInt)
     Console.WriteLine("The Bootloader has been Installed Successfully.");
 else if (Bootloader == 0)
     Console.WriteLine("The Bootloader has not yet been Installed.");
     Console.WriteLine("Installing Bootloader...");
-    Bootloader = 27;
+    Bootloader = VersionInt;
     if (Bootloader == 27)
         Console.WriteLine("The Bootloader has been Installed Successfully.");
-Console.WriteLine("Juno C > ____");
+Console.WriteLine("Juno C >");
+var TemporaryCommand = Console.ReadLine();
+if (TemporaryCommand == "Version")
+{
+    Console.WriteLine(" ");
+    Console.WriteLine("Juno C");
+    Console.WriteLine("Version 27");
+}
+else
+{
+    Console.WriteLine("Command Not Found.");
+}
