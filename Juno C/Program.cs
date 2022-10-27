16796c3b6071b4be57191fb2a17762d8266b1a91
ep300252 <ep300252@CIS-29JPMR2>
2022-10-27 13:31:50 -0400

    Add project files.

---
+++ Juno C/Program.cs
@@ -0,0 +1,12 @@
+﻿var Bootloader = 0;
+Console.WriteLine("Juno C");
+Console.WriteLine("Version 27");
+if (Bootloader == 27)
+    Console.WriteLine("The Bootloader has been Installed Successfully.");
+else if (Bootloader == 0)
+    Console.WriteLine("The Bootloader has not yet been Installed.");
+    Console.WriteLine("Installing Bootloader...");
+    Bootloader = 27;
+    if (Bootloader == 27)
+        Console.WriteLine("The Bootloader has been Installed Successfully.");
+Console.WriteLine("Juno C > ____");
